c3bdd1164586dc664cdf9ef38afc3a4d88ec7ac6
unknown <Minskoleopc>
2024-03-15 20:24:20 +0530

    added

---
+++ script3.js
@@ -0,0 +1,112 @@
+// types 
+
+// introvert  // extrovert
+
+// calm        // loud
+// outing      // more outing
+// less social // more social
+
+// Human 
+
+// Properties  - color , gender , age , height 
+// Methods     - talk() , walk()
+
+// Vehicle 
+// Properties - color , type , model , companyName
+// Methods - start() , stop() 
+
+// Bank acc
+// Properties - accNo , BranchName , accName , Bal
+// Methods - deposit() , withdrawl()
+
+
+let a = 10
+console.log(a)
+console.log(typeof a)
+// 10 , -10 , 19.8 , -89.8
+
+let caDrive = true
+console.log(caDrive)
+console.log(typeof caDrive)
+// True or False
+
+let  first_Name = "chinmay"
+console.log(first_Name)
+console.log(typeof first_Name)
+// "A" , "a" , "234","$#$#" ,"324x54534#@$ , " "
+
+
+// comparison 
+// entity < enity  =======> boolean  ==> true or false
+console.log(typeof '8') // string
+console.log(typeof 8) // number
+
+// < , > , <= , >= 
+// == , !=   ---------> value 
+// === , !== ---------> value and type
+
+console.log(8 === '8')
+console.log(8 === 8)
+console.log(8 !== '8')
+console.log(9 == '9')
+console.log(9 == '90')
+console.log(9 != '9')
+console.log(9 != 9)
+console.log(9 == 9)
+console.log(9 != 9)
+console.log(9 >= 9)
+console.log(9 <= 9)
+console.log(9 > 7)
+console.log(9 < 7)
+
+// program  4
+
+// logical operator  - And --- &&
+
+// true   &&   true  ------> true
+// false  &&   true  ------> false
+// true   &&   false ------> false
+// false  &&   false ------> false
+
+
+
+//  Or - ||
+
+// true   ||   true  ------> true
+// false  ||   true  ------> true
+// true   ||   false ------> true
+// false  ||   false ------> false
+
+
+
+// Not - !
+// not True  --- False 
+// not False  --- True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// comparison operator
+
+ // logical operator 
