c302b95f86ec5ff60b480b07a41478db66c6f558
unknown <Minskoleopc>
2024-03-26 20:21:36 +0530

    added

--- script7.js
+++ script7.js
@@ -118,7 +118,70 @@ for(let i = 1 ; i <= 5 ; i++){ // 2 // 3 // 4 // 5 // 6
     console.log(i) // 1 // 2 // 4 // 5
 }
 
+
+//for(intialization ; conditionCheck ; increment/decrement){
+    // statements
+//}
+
 // while 
+// intialization 
+// while(condition){
+    // statements
+    // increment / decrement
+//}
+
+let i1 = 1
+while(i1 <= 3){
+    console.log(i1) // 1 // 2 // 3
+    i1 = i1 + 1  // 2 // 3 // 4
+}
+
+// print 1 to 5 using while loop
+let i2 = 1
+while(i2 <= 5){
+    console.log(i2) // 1 // 2 // 3 // 4 // 5
+    i2 = i2 + 1  // 2 // 3 // 4 // 5 // 6
+}
+// print hello 3 times 
+let i3 = 1 // 1
+while(i3 <= 3){
+    console.log("hello") // "hello " // "hello" // "hello"
+    i3 = i3 + 1 // 2 // 3 // 4
+} 
+
+// print table of 2
+let i4 = 2
+while(i4 <= 20){
+    console.log(i4) // 2 // 4 // 6 ------------ 20
+    i4 = i4 + 2 // 4 // 6 // 8 -----------------22
+}
+// print table of 5
+let i5 = 5
+while(i5 <= 50){
+    console.log(i5)
+    i5 = i5 + 5
+}
+// print 5 to 1 
+let i6 = 5 
+while(i6 >= 1){
+    console.log(i6) // 5 // 4 // 3 // 2 // 1
+    i6 = i6 - 1
+
+    // cntrl + c
+}
+// table of 5 in reverse
+let i7 = 50
+while(i7 >= 5){
+    console.log(i7)
+    i7 = i7 - 5
+}
+
+
+
+
+
+
+
 
 
 
