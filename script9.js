264fd575f97403e491818f3435c9335d94dce2c5
unknown <Minskoleopc>
2024-03-30 20:29:40 +0530

    added

---
+++ script9.js
@@ -0,0 +1,58 @@
+
+let a = 10
+console.log(a)
+console.log(typeof 10)
+
+let b = "chinmay"
+console.log(b)
+console.log(typeof b)
+
+let c = true
+console.log(c)
+console.log(typeof c)
+
+
+// program 1 
+//             0         1         2        3      4
+let names = ["sarika","poorva","sanchita","raj","ramesh"]
+// array stores the value by index
+console.log(names[0])
+console.log(names[1])
+names[2] = "pranali"
+console.log(names)
+
+// loop
+for(let i = 0 ; i < names.length; i++){ 
+    console.log(names[i]) 
+}
+
+// i - 0
+// i - 1
+// i - 2
+// i - 3
+// i - 4
+
+for(let i = names.length-1 ; i >= 0 ; i-- ){
+    //console.log(i)
+    console.log(names[i])
+}
+
+//              0  1  2  3
+let numbers = [11,22,33,44]
+for(let i = numbers.length-1 ; i >= 0 ; i--){
+    console.log(numbers[i])
+}
+
+// 9 am
+
+
+
+
+
+
+
+
+
+
+
+
