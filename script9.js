6d91edd65beb0d6145d0c7b7d07b53081ca10a33
unknown <Minskoleopc>
2024-03-31 09:35:36 +0530

    added

--- script9.js
+++ script9.js
@@ -43,7 +43,63 @@ for(let i = numbers.length-1 ; i >= 0 ; i--){
     console.log(numbers[i])
 }
 
-// 9 am
+// push ()
+// action - add the element to the last
+// return - mew length of array
+
+let cities = ["pune","mumbai",'banglore',"kolkata"]
+let q1 = cities.push('nagpur')
+console.log(q1)
+console.log(cities)
+
+// unshift()
+// Action - add the element at the start of array 
+// Return - new length of array
+
+let fruits = ["apple","mango","banana","grapes"]
+let q2 = fruits.unshift('chikoo')
+console.log(q2)
+console.log(fruits)
+
+// pop()
+// Action - removes the last element 
+// return - return the same element 
+//            0  1  2  3  4
+let marks = [11,22,33,44,55]
+let q3 = marks.pop()
+console.log(marks)
+console.log(q3)
+
+// shift()
+let state = ["MH","MP","UP"]
+let q4 = state.shift()
+console.log(q4)
+console.log(state)
+
+// shift() , unshift() , push(), pop()
+
+// includes()
+let flowers = ["lily","jasmine","rose","sunflowe"]
+let q5 = flowers.includes('Rose')
+console.log(q5)
+
+// indexOf()
+let q6 = flowers.indexOf('Lily')
+console.log(q6)
+
+// at()
+flowers = ["lily","jasmine","rose","sunflower"]
+let q7 = flowers.at(2)
+console.log(q7)
+
+// push() , unshift(), pop(), shift(), includes() , indexOf(), at()
+
+
+
+
+
+
+
 
 
 
