f40166039ea92f5ab23b9f3106a8f73821c9dd0d
unknown <Minskoleopc>
2024-04-20 20:26:32 +0530

    added

---
+++ DOM/script.js
@@ -0,0 +1,6 @@
+document.querySelector('h1').addEventListener('mouseout',function(){
+    document.querySelector('h1').style.color = "green"
+})
+document.querySelector('h1').addEventListener('mouseover',function(){
+    document.querySelector('h1').style.color = "purple"
+})
