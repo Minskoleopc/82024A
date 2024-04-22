d4c19bb83c0f6fb643619982d68dc71e48596b6c
unknown <Minskoleopc>
2024-04-22 20:38:14 +0530

    added

---
+++ DOM2/script.js
@@ -0,0 +1,9 @@
+// click 
+// dbclick
+// mouseover
+// mouseout 
+// reload
+
+document.querySelector('button').addEventListener('click',function(){
+    document.querySelector('h1').style.color =  "red"
+})
